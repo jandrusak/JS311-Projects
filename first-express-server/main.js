4ef50d9595bed267c4169573010939f081c780f6
jandrusak <jonathanandrusak@gmail.com>
2023-06-01 18:53:39 -0500

    completed

--- first-express-server/main.js
+++ first-express-server/main.js
@@ -3,11 +3,37 @@ let express = require("express");
 
 //create the application object
 let app = express()
+app.use(express.json())
 
-app.get("/", function(request, response){
-    response.send("Hello");
-})
+// GET /add two numbers
+app.get("/add", function(req, res){
+    let num1= parseInt(req.query.num1);
+    let num2= parseInt(req.query.num2);
+    let result = num1+num2; 
+    return res.json({result})
+});
+
+//POST /multiplies 2 numbers
+app.post('/multiply', (req, res) => {
+    const result = {
+     num1: req.body.num1,
+     num2: req.body.num2,
+    };
+   
+    res.json(result.num1 * result.num2);
+   });
+
+
+
+
+
+//GET /returns the negative of a number/Route parameter
+app.get('/negative/:num', (req, res) => {
+    const num = parseInt(req.params.num);
+    const result = -num;
+    return res.json({ result });
+  });
 
 app.listen(9001, function(){
-    console.log("Application started");
+    console.log("Application Running");
 })
